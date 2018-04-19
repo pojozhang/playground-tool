216a9110a837d3fe60c51503db56c0c5fc79a2c8
pojo.zhang <pojo.zhang@gmail.com>
2018-04-19 18:38:46 +0800

    temp commit

--- cmd/create.go
+++ cmd/create.go
@@ -2,40 +2,53 @@ package cmd
 
 import (
 	"github.com/spf13/cobra"
-	"github.com/PuerkitoBio/goquery"
 	"net/http"
 	"strings"
 	"io/ioutil"
 	"fmt"
 	"html"
+	"github.com/berryland/sugar"
 )
 
 var cmdCreate = &cobra.Command{
 	Use:   "create",
 	Short: "Create a problem markdown file",
 	RunE: func(cmd *cobra.Command, args []string) error {
+
+		type data struct {
+		}
 		url := "https://leetcode-cn.com/problems/string-to-integer-atoi/description/"
+		question := extractQuestion(url)
+
+		d := &data{}
+		sugar.Get(questionUrl(question)).Read(d)
 		res, err := http.Get(url)
 		if err != nil {
 			return err
 		}
 		defer res.Body.Close()
 
-		doc, err := goquery.NewDocumentFromReader(res.Body)
-		if err != nil {
-			return err
-		}
-
 		title := extractTitle(doc.Find("title").Text())
 		problem, _ := doc.Find("meta[name='description']").Attr("content")
 		content := buildContent(title, url, problem)
-		fileName := extractFileName(url)
 		ioutil.WriteFile(fileName, []byte(content), 0644)
 		return nil
 	},
 }
 
-func extractFileName(url string) string {
+func questionUrl(title string) string {
+	return fmt.Sprintf(`https://leetcode-cn.com/graphql?query=query getQuestionDetail($titleSlug: String!) {
+  question(titleSlug: $titleSlug) {
+    questionTitle
+    translatedTitle
+    content
+    translatedContent
+  }
+}
+&operationName=getQuestionDetail&variables={"titleSlug":"%s"}`, title)
+}
+
+func extractQuestion(url string) string {
 	const keyword = "problems/"
 	index := strings.Index(url, keyword) + len(keyword)
 	return url[index:strings.Index(url[index:], "/")+index] + ".md"
